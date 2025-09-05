ce380e290553b49869fa0829bc725b6ff0452e27
Tyrone <tyronedk00@hotmail.com>
2025-09-05 16:02:46 +0100

    Added environment variables

--- text_message.py
+++ text_message.py
@@ -5,7 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 account_sid = os.environ.get('ACCOUNT_SID')
 auth_token = os.environ.get('AUTH_TOKEN')
-client = Client('ACcee4cac3f033053cc958f7974eabd9f3', '089bc7f3032ef90e5d6f834f85ba464b')
+from_number = os.environ.get('FROM')
+to_number = os.environ.get('TO')
+client = Client(account_sid, auth_token)
 
 
 def send_message(body) -> MessageInstance:
@@ -15,8 +17,8 @@ def send_message(body) -> MessageInstance:
     :return: Message to send.
     """
     message = client.messages.create(
-        from_='+447488895681',
-        to='+447533487088',
+        from_=from_number,
+        to=to_number,
         body=body
     )
 
